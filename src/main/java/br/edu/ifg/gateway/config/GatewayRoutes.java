ec6d076bb74a2041678c1068bc19128739c4b118
Vitória Silva <silva.lopes.vitoria@gmail.com>
2026-03-27 21:46:23 -0300

    Adicionando docker
    Adicionando arquivos para desenvolvimento em container no docker

--- src/main/java/br/edu/ifg/gateway/config/GatewayRoutes.java
+++ src/main/java/br/edu/ifg/gateway/config/GatewayRoutes.java
@@ -1,6 +1,7 @@
 package br.edu.ifg.gateway.config;
 
 import br.edu.ifg.gateway.filter.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,18 @@ public class GatewayRoutes {
 
     private final JwtAuthFilter jwtFilter;
 
+    @Value("${services.user-url}")
+    private String userUrl;
+
+    @Value("${services.learning-url}")
+    private String learningUrl;
+
+    @Value("${services.trilha-url}")
+    private String trilhaUrl;
+
+    @Value("${services.auth-url}")
+    private String authUrl;
+
     public GatewayRoutes(JwtAuthFilter jwtFilter) {
         this.jwtFilter = jwtFilter;
     }
@@ -20,16 +33,16 @@ public class GatewayRoutes {
         return builder.routes()
                 .route("usuario", r -> r.path("/usuario/**")
                         .filters(f -> f.filter(jwtFilter).addRequestHeader("X-Gateway-Key", "trilhadeaprendizadoapims-gateway"))
-                        .uri("http://localhost:8081"))
+                        .uri(userUrl))
                 .route("learning", r -> r.path("/progresso/**")
                         .filters(f -> f.filter(jwtFilter).addRequestHeader("X-Gateway-Key", "trilhadeaprendizadoapims-gateway"))
-                        .uri("http://localhost:8082"))
+                        .uri(learningUrl))
                 .route("trilha", r -> r.path("/trilha/**")
                         .filters(f -> f.filter(jwtFilter).addRequestHeader("X-Gateway-Key", "trilhadeaprendizadoapims-gateway"))
-                        .uri("http://localhost:8083"))
+                        .uri(trilhaUrl))
                 .route("autenticacao", r -> r.path("/auth/**")
                         .filters(f -> f.addRequestHeader("X-Gateway-Key", "trilhadeaprendizadoapims-gateway"))
-                        .uri("http://localhost:8084"))
+                        .uri(authUrl))
                 .build();
     }
 }
